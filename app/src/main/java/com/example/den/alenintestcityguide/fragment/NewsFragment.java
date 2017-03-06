4d4081683f9d232d59ebf59700aa785a4a5e42da
Roman Laktionov <r.a.laktionov@gmail.com>
2017-03-06 16:15:05 +0200

    Background download in News Fragment

--- app/src/main/java/com/example/den/alenintestcityguide/fragment/NewsFragment.java
+++ app/src/main/java/com/example/den/alenintestcityguide/fragment/NewsFragment.java
@@ -12,15 +12,18 @@ import android.view.ViewGroup;
 import com.example.den.alenintestcityguide.model.NewsItem;
 import com.example.den.alenintestcityguide.R;
 import com.google.gson.Gson;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonPrimitive;
 import com.google.gson.JsonSyntaxException;
 
 import org.apache.commons.io.IOUtils;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 
 
 /**
@@ -28,82 +31,65 @@ import java.net.URL;
  */
 public class NewsFragment extends Fragment {
 
-    private static final String URL_PATH = "http://www.056.ua/apitest/newstest";
-    private URL url;
-    public NewsFragment() {
+    public static final String TAG = "NEWS FRAGMENT";
+    private List<NewsItem> newsItems_list;
 
+    public NewsFragment() {
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        try {
-            url = new URL(URL_PATH);
-        } catch (MalformedURLException e) {
-            e.printStackTrace();
-        }
-        new NewsLoading().execute(url);
-        return inflater.inflate(R.layout.fragment_news, container, false);
+        View root = inflater.inflate(R.layout.fragment_news, container, false);
+
+        new NewsLoading().execute();
+
+        return root;
 
     }
 
 
-    private class NewsLoading extends AsyncTask<URL,Void,NewsItem> {
+    private class NewsLoading extends AsyncTask<Void, Void, List<NewsItem>> {
+        private static final String URL_PATH = "https://www.056.ua/apitest/newstest";
+
         private Gson gson;
-
-
+        private URL url;
 
         public NewsLoading() {
             this.gson = new Gson();
+            newsItems_list = new ArrayList<>();
         }
 
         @Override
-        protected void onPreExecute() {
-            super.onPreExecute();
-
-        }
-
-
-        @Override
-        protected NewsItem doInBackground(URL... urls) {
-            BufferedReader reader = null;
+        protected List<NewsItem> doInBackground(Void... voids) {
             try {
-                String json = IOUtils.toString(url);
-                reader = new BufferedReader(new InputStreamReader(url.openStream()));
-                StringBuffer buffer = new StringBuffer();
-                int read;
-                char[] chars = new char[1024];
-                while ((read = reader.read(chars)) != -1)
-                    buffer.append(chars, 0, read);
-
-//                return buffer.toString();
-                Log.d("!!!",buffer.toString());
-//                return gson.fromJson(IOUtils.toString(urls[0].openStream()), NewsItem.class);
+                url = new URL(URL_PATH);
+                JsonObject jsonObject = gson.fromJson(IOUtils.toString(url.openStream()), JsonObject.class);
+                Log.d(TAG, "JSON OBJECT " + jsonObject.toString());
+                JsonArray jsonArray = jsonObject.getAsJsonArray("response");
+                for (JsonElement element : jsonArray) {
+                    NewsItem newsItem = gson.fromJson(element.toString(), NewsItem.class);
+                    newsItems_list.add(newsItem);
+                }
             } catch (JsonSyntaxException | IOException e) {
-                e.printStackTrace();
+                Log.d(TAG, "ERROR IN " + e.toString());
             }
-            return null;
+
+            return newsItems_list;
         }
 
 
         @Override
-        protected void onPostExecute(NewsItem newsItem) {
-            super.onPostExecute(newsItem);
-//            Log.d("!!!",newsItem.toString());
-        }
-
-        @Override
-        protected void onProgressUpdate(Void... values) {
-            super.onProgressUpdate(values);
-
+        protected void onPostExecute(List<NewsItem> list) {
+            // notify adapter about changes
+            //  or set adapter here!
+            Log.d(TAG, "LIST OF NEWS" + list.size());
         }
     }
 
